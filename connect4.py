e83447289c92d6ddd6c52798dc7c44a391142cad
Aaron Perley <aaron.perley@gmail.com>
2014-10-27 17:13:33 -0400

    Add connect4 interface

--- connect4.py
+++ connect4.py
@@ -3,3 +3,12 @@
 # Aaron Perley (aperley)
 #
 # For AI Optional Lecture for 15-112 F14
+
+class Connect4State(object):
+    def __init__(self, rows, cols):
+        pass
+
+    # 0 = empty, 1 = player 1, 2 = player 2
+    # returns [(childState, column)]
+    def getChildStates(self, currentPlayer):
+        pass
