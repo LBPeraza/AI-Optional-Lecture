51037a3507023bb5423bd1cb3b74001e660ddf49
LBPeraza <lukasperaza@gmail.com>
2014-10-27 21:57:13 -0400

    Minimax
    (I really don't know if this is going to work at all...)

--- connect4.py
+++ connect4.py
@@ -4,6 +4,8 @@
 #
 # For AI Optional Lecture for 15-112 F14
 
+WINVALUE = 10000
+
 class Connect4State(object):
     def __init__(self, rows, cols):
         pass
@@ -17,13 +19,29 @@ class Connect4State(object):
     def getScore(self, currentPlayer):
         pass
 
-# returns (Connect4State, column) of best move
-def minimax(board, player, depth=5, maximizing=True):
+# returns column of move which minimizes maximum loss
+def minimax(board, player, depth=5):
+    state, column, score = minimaxAlgo(board, player, depth)
+    return column
+
+# returns (Connect4State, column, score) of best move
+def minimaxAlgo(board, player, depth=5, maximizing=True):
     if (depth < 1): raise Exception("minimax error")
     elif (depth == 1):
         return board
     else:
         choice = None
+        choiceCol = None
         choiceScore = None
         for childState, column in board.getChildStates():
-            
+            childScore = childState.getScore(player)
+            if abs(childScore) == WINVALUE:
+                return childState, column, childScore
+            childScore = minimaxAlgo(board, player, depth-1, not maximizing)
+            if (choice == None or
+                (maximizing and childScore > choiceScore) or
+                (not maximizing and childScore < choiceScore)):
+                choice = childState
+                choiceCol = column
+                choiceScore = childScore
+        return choice, choiceCol, choiceScore
